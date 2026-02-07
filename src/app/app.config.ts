e3bf7f7d91d2e627b55913c69f6480be43d88bc8
yanS <yansilvv2227@gmail.com>
2026-02-07 18:01:49 -0300

    Projeto inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { ControleEditora } from './controle-editora';
+import { ControleLivros } from './controle-livros';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    ControleEditora,
+    ControleLivros
   ]
 };
